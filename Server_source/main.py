bb5cc02a1fbe02c8bbce23c1d0f638df45101869
Szymon <szflank@gmail.com>
2025-05-16 18:01:23 +0200

    Added labelerService

--- Server_source/main.py
+++ Server_source/main.py
@@ -1,9 +1,27 @@
 import sys
 
+from services.LabelerService import LabelerService
 from services.DataDownloaderService import DataDownloaderService
+from services.FloodCheckerService import FloodCheckerService
+
+verify = False
 
 data_input_path = './data_input'
 
 dataDownloaderService = DataDownloaderService()
 
-dataDownloaderService.download_data(data_input_path)
+path_to_input_folder = dataDownloaderService.download_data(data_input_path)
+
+labelerService = LabelerService()
+floodCheckerService = FloodCheckerService()
+
+if verify:
+    labelerService.label_tweets(path_to_input_folder)
+    labelerService.label_rss(path_to_input_folder)
+    floodCheckerService.indentify(path_to_input_folder)
+else:
+    floodCheckerService.indentify(path_to_input_folder)
+
+
+
+# floodCheckerService.indentify(path_to_input_folder)
---
+++ Server_source/services/LabelerService.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+class LabelerService():
+    
+
+    def label_tweets(self,path_to_input_folder:str) -> None:
+        path_to_rss_file = path_to_input_folder + '/tweeter_messages.csv'
+        self.add_labels(path_to_rss_file)
+
+    def label_rss(self,path_to_input_folder:str) -> None:
+         path_to_rss_file = path_to_input_folder + '/rss_messages.csv'
+         self.add_labels(path_to_rss_file)
+
+    def add_labels(self,path_to_file:str) -> None:
+        try:
+            df = pd.read_csv(path_to_file)
+
+            is_about_flood = []
+
+            for index, row in df.iterrows():
+
+                incorrect = True
+                while(incorrect):
+                    print('=== MESSAGE:')
+                    print(row['message'])
+                    print('=== IS ABOUT FLOOD?:')
+                    response = input()
+                    print('=== === === === ===:')
+
+                    if response.isdigit() and (int(response) == 0 or int(response) == 1):
+                        is_about_flood.append(response)
+                        incorrect = False
+                    else:
+                         print('Wrong value! Repeat answear')
+                         incorrect = True
+                     
+
+            df.insert(loc = 0, column = 'is_about_flood', value = is_about_flood)
+
+            df.to_csv(path_to_file, index=False, encoding="utf-8",sep=';', quoting=1)
+
+        except Exception as e:
+                print(f"An error occurred: {e}")
+
+
